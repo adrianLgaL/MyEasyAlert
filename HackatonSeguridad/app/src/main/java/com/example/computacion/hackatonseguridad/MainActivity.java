f18c017ce2967c1af4434c0b1652edd1b37f46fb
adrianLgaL <2015030911@upsin.edu.mx>
2018-04-16 21:37:49 -0600

    mensaje

---
+++ HackatonSeguridad/app/src/main/java/com/example/computacion/hackatonseguridad/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.computacion.hackatonseguridad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView txtCurp, txtContra;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtCurp = (TextView)findViewById(R.id.txtCurp);
+        txtContra = (TextView)findViewById(R.id.txtContra);
+
+
+    }
+
+    public void iniciarSesion(View view){
+        if(txtCurp.getText().toString().equals("")||txtContra.getText().toString().equals("")){
+            Toast.makeText(this, "Llenar espacios vacios", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            Intent i = new Intent(this,Menu.class);
+            startActivity(i);
+            finish();
+        }
+    }
+}
---
+++ HackatonSeguridad/app/src/main/java/com/example/computacion/hackatonseguridad/Menu.java
@@ -0,0 +1,28 @@
+package com.example.computacion.hackatonseguridad;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Menu extends AppCompatActivity {
+
+    private String[] contactos={"Ambulancia","Salvavidas","Policia","Extravio"};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+    }
+
+    public void salir(View view){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
